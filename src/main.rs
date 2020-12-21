aa4f338414a261320284ee9e902db380c42dd9bb
Constantin Gierczak--Galle <constantin.gierczak-galle@ens.fr>
2020-12-21 10:14:44 +0100

    Naive ready

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::collections::HashMap;
+
+fn naive(input : &str, pattern : &str) -> usize {
+    let input_bytes = input.as_bytes();
+    let pattern_bytes = pattern.as_bytes();
+    let n = input_bytes.len();
+    let m = pattern_bytes.len();
+    let mut possible;
+    let mut occ = 0;
+    for i in 0..(n - m + 1) {
+        possible = true;
+        for j in 0..m {
+            if input_bytes[i + j] != pattern_bytes[j] {
+                possible = false;
+            }
+        }
+        if possible {
+            occ += 1;
+        }
+    }
+    occ
+}
+
+struct Trie {
+    value: usize,
+    children : HashMap<char, Trie>
+}
+
+fn construct_trie(trie : &mut Trie, string : &str, value : usize) -> () {
+    
+}
+
 fn main() {
-    println!("Hello, world!");
+    let text = String::from("abcd dcba ab ab");
+    let pattern = String::from("ab");
+    println!("Number of occurences : {}", naive(&text, &pattern));
 }
